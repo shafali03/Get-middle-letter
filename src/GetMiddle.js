bc5d198eed0dfd3ff0c5d3564360acd275c03860
shafali03 <shaf03sm@gmail.com>
2020-02-01 22:32:41 +0000

    test passing for odd letter

--- src/GetMiddle.js
+++ src/GetMiddle.js
@@ -4,3 +4,10 @@
 //   this.currentlyPlayingSong = song;
 //   this.isPlaying = true;
 // };
+
+GetMiddle.prototype.getLetter = function(str) {
+
+  var middle = Math.floor(str.length / 2)
+    return str.charAt(middle -1)
+  
+}
