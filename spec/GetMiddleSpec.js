53dd9380888f9d0869c027022361b909e97bc280
shafali03 <shaf03sm@gmail.com>
2020-02-01 22:43:42 +0000

    deleted commits

--- spec/GetMiddleSpec.js
+++ spec/GetMiddleSpec.js
@@ -1,11 +1,4 @@
-// get_middle("test") # => "es"
-// get_middle("testing") # => "t"
-// get_middle("middle") # => "dd"
-// get_middle("A") # => "A"
-// get_middle("of") # => "of"
-
 describe('GetMiddle', function() {
-
   var getMiddle 
 
   //only show one letter for odd
