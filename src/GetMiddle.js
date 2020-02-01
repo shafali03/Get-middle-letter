ca8f0c9a76447577907c2521dccfbc2d0da44f6f
shafali03 <shaf03sm@gmail.com>
2020-02-01 22:46:07 +0000

    unnecessary comment

--- src/GetMiddle.js
+++ src/GetMiddle.js
@@ -1,10 +1,3 @@
-// function Player() {
-// }
-// Player.prototype.play = function(song) {
-//   this.currentlyPlayingSong = song;
-//   this.isPlaying = true;
-// };
-
 GetMiddle.prototype.getLetter = function(str) {
 
   var middle = Math.floor(str.length / 2)
