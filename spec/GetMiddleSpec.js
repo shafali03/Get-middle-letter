db6bc8b81aa606f99371ad9af32d2f82ed4c30d0
shafali03 <shaf03sm@gmail.com>
2020-02-01 22:42:21 +0000

    added few more test, all test are passing for middle letters

--- spec/GetMiddleSpec.js
+++ spec/GetMiddleSpec.js
@@ -6,18 +6,30 @@
 
 describe('GetMiddle', function() {
 
-
   var getMiddle 
 
   //only show one letter for odd
-
   it('should show middle letter for "testing"', function() {
     getMiddle = new GetMiddle();
     expect(getMiddle.getLetter('testing')).toEqual('t')
   })
 
+  it('should show middle letter for "A"', function() {
+    getMiddle = new GetMiddle();
+    expect(getMiddle.getLetter('A')).toEqual('A')
+  })
 
   // only show two letter of even
+  it('should show middle letters for "middle"', function() {
+    getMiddle = new GetMiddle();
+    expect(getMiddle.getLetter('middle')).toEqual('dd')
+  })
+
+  it('should show middle letters for "of"', function() {
+    getMiddle = new GetMiddle();
+    expect(getMiddle.getLetter('of')).toEqual('of')
+  })
+
   it('should show middle letters for "test"', function() {
     getMiddle = new GetMiddle();
     expect(getMiddle.getLetter('test')).toEqual('es')
